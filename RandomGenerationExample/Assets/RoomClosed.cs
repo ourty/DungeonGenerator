b434ec670a01d02f0df57c649d3f336c499b9d8f
Aristotle Katimbang <kat.aristotle@yahoo.com>
2020-03-07 21:36:27 -0500

    Update RoomClosed.cs

--- RandomGenerationExample/Assets/RoomClosed.cs
+++ RandomGenerationExample/Assets/RoomClosed.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//hello
 public class RoomClosed : MonoBehaviour
 {
     public List<char> closingNeeded = new List<char>();
